e6e6e86a68701514e656d35e15cfca254be38c45
Carla Evellyn Ferreira dos Santos <129027590+Carlaevellyn@users.noreply.github.com>
2026-06-09 01:35:21 -0300

    Add files via upload

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { SidebarLayout } from "../_components/sidebar-layout";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarLayout>
+      {children}
+    </SidebarLayout>
+  );
+}
